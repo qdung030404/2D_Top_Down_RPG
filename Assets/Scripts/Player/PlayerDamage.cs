28ea247b82d7063a368074de56fe6a992354de0a
qdung030404 <qdung030404@gmail.com>
2025-10-28 21:07:03 +0700

    Sword Collision Enemy Health

--- Assets/Scripts/Player/PlayerDamage.cs
+++ Assets/Scripts/Player/PlayerDamage.cs
@@ -6,10 +6,7 @@ public class PlayerDamage : MonoBehaviour
     [SerializeField] private int damage = 1;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.GetComponent<EnemyHP>())
-        {
             EnemyHP enemyHp = other.gameObject.GetComponent<EnemyHP>();
-            enemyHp.TakeDamage(damage);
-        }
+            enemyHp?.TakeDamage(damage);
     }
 }
